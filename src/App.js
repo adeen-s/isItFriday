2aa0053863f7e22002894b6fd3b9da6a3b45ec6a
Adeen Shukla <adeen@adeen.me>
2022-07-30 00:18:49 +0530

    Not a friday anymore. fix the code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   // function to check if today is friday
+  const [res, setRes] = useState("No");
+
   const isFriday = () => {
     const today = new Date();
     const day = today.getDay();
     return day === 5;
   };
 
+  useEffect(() => {
+    if (isFriday()) {
+      setRes("Yes");
+    }
+  }
+  , []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h4>Is It Friday?</h4>
-        <h4>{isFriday ? "Yes" : "No"}</h4>
+        <h4>{res}</h4>
       </header>
     </div>
   );
