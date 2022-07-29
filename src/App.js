20b3b55155bceca4c5da84d68e1b98262059d526
Adeen Shukla <adeen@adeen.me>
2022-07-29 22:26:02 +0530

    Is it Friday?

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  // function to check if today is friday
+  const isFriday = () => {
+    const today = new Date();
+    const day = today.getDay();
+    return day === 5;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h4>Is It Friday?</h4>
+        <h4>{isFriday ? "Yes" : "No"}</h4>
       </header>
     </div>
   );
